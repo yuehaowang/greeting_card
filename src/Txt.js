5b562eeb7ecc997df27612addcca8032c62bae4c
yuehaowang <wangyuehao1999@gmail.com>
2018-02-22 15:14:42 +0800

    Enhanced preface: word wrap.

--- src/Txt.js
+++ src/Txt.js
@@ -2,7 +2,6 @@ function Txt (text) {
 	var s = this;
 
 	s.x = config.stageW / 2;
-	s.y = config.stageH / 2;
 	s.text = text || "";
 	s.visible = true;
 }
@@ -19,8 +18,53 @@ Txt.prototype = {
 		ctx.fillStyle = "#ECECEC";
 		ctx.font = "bold 20pt sans-serif";
 		ctx.textAlign = "center";
-		ctx.textBaseline = "middle";
-		ctx.fillText(s.text, s.x, s.y);
+		ctx.textBaseline = "top";
+
+		var lines = s.getTextLines();
+		var lineH = ctx.measureText("O").width * 2.4, top = (config.stageH - (lines.length * lineH)) * 0.5;
+
+		for (var i = 0, l = lines.length; i < l; i++) {
+			var ln = lines[i];
+
+			ctx.fillText(ln, s.x, top + lineH * i);
+		}
+
 		ctx.restore();
+	},
+
+	getTextLines() {
+		var tokens = this.text.match(/(\S+)(\s*)/gi);
+		var maxWidth = config.stageW - 40;
+		var temp = "", lines = new Array();
+
+		for (var j = 0, l = tokens.length; j < l; j++) {
+			var t = tokens[j];
+
+			if (ctx.measureText(temp + t).width > maxWidth) {
+				if (ctx.measureText(t).width > maxWidth) {
+					for (var i = 0, m = t.length; i < m; i++) {
+						if (ctx.measureText(temp + t[i]).width > maxWidth) {
+							lines.push(temp);
+
+							temp = "";
+						} else {
+							temp += t[i];
+						}
+					}
+				} else {
+					lines.push(temp);
+
+					temp = t;
+				}
+			} else {
+				temp += t;
+			}
+		}
+
+		if (temp.length > 0) {
+			lines.push(temp);
+		}
+
+		return lines;
 	}
 };
